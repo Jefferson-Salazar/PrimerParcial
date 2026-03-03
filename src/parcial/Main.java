fee1a54c90b07c4beba30d840942da014e5a9fe0
Jefferson Osberto Salazar Escobar <jsalazare4@miumg.edu.gt>
2026-03-03 08:56:39 -0600

    Segunda prueba de commit

--- src/parcial/Main.java
+++ src/parcial/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//Prueba de commit
+		//Prueba de commit
+		//Segunda prueba de commit
 	}
 
 }
